66e60e4dabb8b5be4c66a91d144846428dc7f912
por964 <claesvonh@gmail.com>
2022-02-28 11:14:55 +0100

    my_test

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ Routes:
 -----------------------------------------------------------------""" 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", text="Dette er en hest!!!")
 
 
 """ ----------------------------------------------------------------
